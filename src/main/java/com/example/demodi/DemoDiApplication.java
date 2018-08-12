d2fc3932506602aeb4f0cd8ac4af39c2505c95e1
PawelKowalewski1 <p.m.kowalewski@gmail.com>
2018-08-12 10:05:58 +0200

    pierwszy commit spring

---
+++ src/main/java/com/example/demodi/DemoDiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDiApplication.class, args);
+	}
+}
